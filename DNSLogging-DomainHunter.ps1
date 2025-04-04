2813ea0bea6f395fd6af27ef98d0bfed361f6c00
TTS-Sully <esullivan@techteamsolutions.com>
2025-04-04 11:52:20 -0400

    updating for date time

--- DNSLogging-DomainHunter.ps1
+++ DNSLogging-DomainHunter.ps1
@@ -1,6 +1,12 @@
 # Define the domain to search for
 $domain = "btloader.com"
 
+# Get Curent TD
+$currentDate = Get-Date
+
+# Format the date as year number, month number, day number
+$formattedDate = $currentDate.ToString("yyyy-MM-dd-HH-mm-ss")
+
 # Get DNS client events from the operational log
 $events = Get-WinEvent -LogName "Microsoft-Windows-DNS-Client/Operational" -FilterXPath "*[System/EventID=3006]"
 
@@ -8,7 +14,7 @@ $events = Get-WinEvent -LogName "Microsoft-Windows-DNS-Client/Operational" -Filt
 $filteredEvents = $events | Where-Object { $_.Message -like "*$domain*" }
 
 # Define the CSV file path
-$csvFilePath = "dns_client_events.csv"
+$csvFilePath = "dns_client_events" + $formattedDate + ".csv"
 
 # Export the filtered events to a CSV file
 $filteredEvents | Select-Object TimeCreated, Message | Export-Csv -Path $csvFilePath -NoTypeInformation
