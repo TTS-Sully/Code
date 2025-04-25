d411be90506d1662aaf229100ea0d2d9258c28a6
TTS-Sully <esullivan@techteamsolutions.com>
2025-04-25 14:24:27 -0400

    Update RemoveDuplicates.ps1
    Comment and Completed

--- RemoveDuplicates.ps1
+++ RemoveDuplicates.ps1
@@ -8,7 +8,9 @@
 
 # Get all files in the folder
 $files = Get-ChildItem -File
+$totalremoved = 0
 
+# Create a hash table to store the base names and their corresponding file objects
 $dataFORMATTED = $files | ForEach-Object {
     $hash = Get-FileHash -Algorithm MD5 -Path $_.FullName
     [PSCustomObject]@{
@@ -17,21 +19,28 @@ $dataFORMATTED = $files | ForEach-Object {
         CreationTime = $_.CreationTime
     }
 }
-    
-$dataFORMATTED | Sort-Object -Property CreationTime | Group-Object -Property Hash | ForEach-Object {
-    if($_.Count -gt 1){
-        Write-Host "Duplicate files found:" -ForegroundColor Yellow
 
-        #$_.Group | Format-List *
+# Format the data sort by CreatuionTime and group by Hash
+$dataFORMATTED | Sort-Object -Property CreationTime | Group-Object -Property Hash | ForEach-Object {
+    # Check if there are duplicates (more than one file with the same hash)
+    if($_.Count -gt 1){
+        # Write-Host "Duplicate files found:" -ForegroundColor Yellow
+        # Find the earliest file based on CreationTime and remove the rest
+        $earliestFile = $_.Group | Sort-Object -Property CreationTime | Select-Object -First 1
+
+        # Take the group of files keep the earliest file and remove the rest
         $_.Group | ForEach-Object {
             if ($_.Path -ne $earliestFile.Path){
-                $local = Get-Item $_.Path #| Format-List -Property Name, CreationTimeUtc
-                Write-Host $local.CreationTimeUtc  $local.Name -ForegroundColor Red
+                # Write-Host $_.CreationTime  $_.Path -ForegroundColor Red
+                $_ | Remove-Item -Force -ErrorAction SilentlyContinue
+                $totalremoved++
             } else {
-                Write-Host $earliestFile.CreationTimeUtc $earliestFile.Path -ForegroundColor Green
+                # Write-Host $_.CreationTime  $_.Path -ForegroundColor Green
             }
         }
     } else {
-        $_.Group | ForEach-Object { Write-Host "No duplicates found for: " $_.Path -ForegroundColor Green }
+        #$_.Group | ForEach-Object { Write-Host "No duplicates found for: " $_.Path -ForegroundColor Green }
     }
 }
+
+Write-Host "Total files removed: $totalremoved" -ForegroundColor Green
