28e7b7dd746e691ef6890dad6cce519c78e7169e
TTS-Sully <esullivan@techteamsolutions.com>
2025-07-16 14:14:26 -0400

    Update CheckAllLogsForEventID.ps1
    Converted the search window from Days to Hours

--- CheckAllLogsForEventID.ps1
+++ CheckAllLogsForEventID.ps1
@@ -4,20 +4,25 @@
 ### Written by ESS
 ##########################################################################################################################
 # Define the event ID you want to search for
-#$EventID = $env:EventIDToFind
-$EventID = 2
+$EventID = $env:EventIDToFind
+#$EventID = 2
+$EventWindow = $env:EventWindow #in Hours
+#$EventWindow = 24
 
 # Get all event logs on the system
 $EventLogs = Get-EventLog -List
 
 # Loop through each event log and search for the event ID
 foreach ($Log in $EventLogs) {
-    $Events = Get-EventLog -LogName $Log.Log -InstanceId $EventID -ErrorAction SilentlyContinue
+    
+$Events = Get-EventLog -LogName $Log.Log -InstanceId $EventID -After (Get-Date).AddHours(-($EventWindow)) -ErrorAction SilentlyContinue
+
     if ($Events) {
-        #Write-Host "Found events with the specified ID"
+        Write-Host "Found events with the specified ID"
         exit 0
     } else {
         #Write-Host "Found no events with the specified ID in this log"
     }
 }
+Write-Host "Found No Events"
 exit 1
