0d3f03a3ed40f4b3f08b1a23b31ce74a7704cf48
TTS-Sully <esullivan@techteamsolutions.com>
2025-07-24 15:42:12 -0400

    A lot of work

--- Fix-Win5000ErrorFix.ps1
+++ Fix-Win5000ErrorFix.ps1
@@ -7,6 +7,10 @@
 #Installs Windows Security and Resets the package
 Get-AppxPackage *Microsoft.SecHealthUI* -AllUsers | Reset-AppxPackage
 
+# Enables Windows Defender in local group policy
+Set-ItemProperty -Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows Defender' -Name DisableAntiSpyware -Value 0
+Set-Service -Name WinDefend -StartupType Automatic
+
 ###########################################################################################################################
 #Enable Memory Integrity
 $path = "HKLM:\SYSTEM\CurrentControlSet\Control\DeviceGuard\Scenarios\HypervisorEnforcedCodeIntegrity"
@@ -35,11 +39,23 @@ Set-ItemProperty -Path $path -Name "(Default)" -Value 1
 
 ###########################################################################################################################
 # Enable Windows Defender App and Browser Control
-# Loads the default user registry hive
-reg load HKLM\DEFAULT c:\users\default\ntuser.dat
+# Enable SmartScreen for File Explorer (Check apps and files)
+# This sets a local group policy equivalent to enable SmartScreen for checking apps and files.
+$explorerPolicyPath = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\System"
+if (-not (Test-Path $explorerPolicyPath)) {
+    New-Item -Path $explorerPolicyPath -Force | Out-Null
+}
+Set-ItemProperty -Path $explorerPolicyPath -Name "EnableSmartScreen" -Value 1 -Type DWord
 
-# Turn on app and browser control
-reg add "HKLM\DEFAULT\Software\Microsoft\Windows\CurrentVersion\Explorer" /v SmartScreenEnabled /t REG_DWORD /d 1 /f
+# Enable SmartScreen for Microsoft Edge (optional)
+$edgePolicyPath = "HKLM:\SOFTWARE\Policies\Microsoft\Edge"
+if (-not (Test-Path $edgePolicyPath)) {
+    New-Item -Path $edgePolicyPath -Force | Out-Null
+}
+Set-ItemProperty -Path $edgePolicyPath -Name "SmartScreenEnabled" -Value 1 -Type DWord
 
-# Unload the default user registry hive
-reg unload HKLM\DEFAULT
+#Turn on Blocking of potentially unwanted applications (PUA)
+#New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows Defender\MpEngine" -Force
+#Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows Defender\MpEngine" -Name "MpEnablePus" -Value 1 -Type DWord
+
+
