7d9133af6d62f9f74726a549de215faa564a289d
TTS-Sully <esullivan@techteamsolutions.com>
2025-08-21 12:05:04 -0400

    Create install_SecretStoreModules.ps1

---
+++ install_SecretStoreModules.ps1
@@ -0,0 +1,8 @@
+##########################################################################################################################
+### Tech Team Solutions - Install Secret Store Modules
+### Last Updated 2025.0819
+### Written by ESS
+##########################################################################################################################
+
+Install-Module -Name Microsoft.Powershell.SecretStore -Scope CurrentUser -Force
+Install-Module -Name Microsoft.Powershell.SecretManagement -Scope CurrentUser -Force
