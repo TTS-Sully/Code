25d93a00512fce912c8f8908e96f1434a17ac680
TTS-Sully <esullivan@techteamsolutions.com>
2025-08-25 14:17:50 -0400

    Create report_CollectLockoutAccounts.ps1

---
+++ report_CollectLockoutAccounts.ps1
@@ -0,0 +1,19 @@
+#Collect lockout accounts from ADS
+
+$logname = "security"
+$dcname = (Get-AdDomain).pdcemulator
+$eventID = "4740"
+$content = Get-EventLog -LogName $logname -ComputerName $dcname -After (Get-Date).AddDays(-1) -InstanceId $eventID | Select TimeGenerated, ReplacementStrings
+$ofs = "`r`n`r`n"
+$body = "Fetching event log started on " + (Get-Date) + $ofs
+
+If ($content -eq $null) {
+    $body = $body + "No lock-out accounts happened today" + $ofs
+} Else {
+    Foreach ($ofevent in $content) {
+        $source = $content.ReplacementStrings[1]
+        $username = $content.ReplacementStrings[0]
+        $body = $body + $ofevent.TimeGenerated + ": " + $username + " - " + $source + $ofs
+    }
+}
+$body
